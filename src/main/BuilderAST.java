30dc4acdb5a5ff854e2abb240f1f7ef1ea6436ff
amnv <amnv@cin.ufpe.br>
2017-04-25 20:41:48 -0300

    Added functionalities to BuilderAst and some others small changes

--- src/main/BuilderAST.java
+++ src/main/BuilderAST.java
@@ -1,10 +1,106 @@
 package main;
 
-import ast.Program;
+import java.util.List;
+
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import ast.*;
+import main.amnv_jrmParser.*;
+import visitor.*;
 
 public class BuilderAST {
-	public Program visitGoal()
+	
+	public Program visit(GoalContext m)
 	{
-		return new Program(null, null);
+		return new Program(this.visitMainClass(m.mainClass()), this.visitClassDeclaration(m.classDeclaration()));
+	}
+
+	public ClassDeclList visitClassDeclaration(List<ClassDeclarationContext> classDeclaration) {
+		ClassDeclList cdl = new ClassDeclList();
+		for (int i = 0; i < classDeclaration.size(); i++)
+		{
+			cdl.addElement(this.visitClassDecl(classDeclaration.get(i)));
+		}
+		return cdl;
+	}
+
+	public MainClass visitMainClass(MainClassContext mainClass) {
+		Identifier i1 = new Identifier(mainClass.IDENTIFIER(0).toString());
+		Identifier i2 = new Identifier(mainClass.IDENTIFIER(1).toString());
+		Statement st = this.visitStatement(mainClass.statement());
+		return new MainClass(i1, i2, st);
+	}
+	
+	public ClassDecl visitClassDecl(ClassDeclarationContext c)
+	{
+		List<TerminalNode> id = c.IDENTIFIER();
+		VarDeclList vdl = this.visitVarDeclarationContext(c.varDeclaration());
+		MethodDeclList mdl = this.visitMethodoDeclarationContext(c.methodDeclaration ());
+		
+		if (id.size() > 1)
+		{
+			Identifier id1 = this.visitIdentifier(c.IDENTIFIER(0));
+			Identifier id2 = this.visitIdentifier(c.IDENTIFIER(1));	
+			return new  ClassDeclExtends(id1, id2, vdl,mdl);
+		}
+		
+		return new ClassDeclSimple(this.visitIdentifier(id.get(0)), vdl, mdl);
+	}
+	
+	public MethodDeclList visitMethodoDeclarationContext(List<MethodDeclarationContext> m)
+	{
+		return new MethodDeclList();
+	}
+	
+	public VarDeclList visitVarDeclarationContext(List<VarDeclarationContext> v)
+	{
+		VarDeclList var = new VarDeclList();
+		
+		for (int i = 0; i < v.size(); i++) {
+			var.addElement(this.visitVarDecl(v.get(i)));
+		}
+		
+		return var;
+	}
+	
+	public VarDecl visitVarDecl(VarDeclarationContext v)
+	{
+		Type at = this.visitTypeContext(v.type());
+		Identifier ai = this.visitIdentifier(v.IDENTIFIER());
+		return new VarDecl(at, ai);
+	}
+	
+	public Type visitTypeContext(TypeContext t)
+	{
+		String type = t.getText();
+		Type ret;
+		if (type.contains("int"))
+		{
+			ret = new IntegerType();
+		} 
+		else if (type.contains("int[]"))
+		{
+			ret = new IntArrayType();
+		}
+		else if (type.contains("boolean"))
+		{
+			ret = new BooleanType();
+		}
+		else ret = new IdentifierType(t.IDENTIFIER().getText());
+		
+		return ret;
+	}
+	
+	public Identifier visitIdentifier(TerminalNode t)
+	{
+		return new Identifier("");
+	}
+	
+	
+	
+	
+	public Statement visitStatement(StatementContext st)
+	{		
+		return new If(null,null, null);
 	}
 }
--- src/main/Start.java
+++ src/main/Start.java
@@ -15,18 +15,18 @@ public class Start {
 
 	public static void main(String[] args) {
 		try {
-		InputStream entrada = new FileInputStream("And.java");
-		ANTLRInputStream input = new ANTLRInputStream(entrada);
-		amnv_jrmLexer lexer = new amnv_jrmLexer(input);
-		CommonTokenStream token = new CommonTokenStream(lexer);
-		amnv_jrmParser parser = new amnv_jrmParser(token);
-		
-		//ajeitar a partir daqui
-		Program program = new BuilderAST().visitGoal();
-		
-		PrettyPrintVisitor visitor =  new PrettyPrintVisitor();
-		//visitor.visit(program);
-		entrada.close();
+			InputStream entrada = new FileInputStream("And.java");
+			ANTLRInputStream input = new ANTLRInputStream(entrada);
+			amnv_jrmLexer lexer = new amnv_jrmLexer(input);
+			CommonTokenStream token = new CommonTokenStream(lexer);
+			amnv_jrmParser parser = new amnv_jrmParser(token);
+			
+			//ajeitar a partir daqui
+			Program prog = new BuilderAST().visit(parser.goal());
+			
+			prog.accept(new PrettyPrintVisitor());
+			//visitor.visit(program);
+			entrada.close();
 		} catch (FileNotFoundException e) {}
 		catch (IOException e) {
 			// TODO Auto-generated catch block
